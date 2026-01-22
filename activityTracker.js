5bfe2975222a696e3de46d347a4864745502b91d
Nina <janev_nina_alexandra@student.smc.edu>
2026-01-22 11:50:08 -0800

    added weekly activity data

---
+++ activityTracker.js
@@ -0,0 +1,60 @@
+// activityTracker.js
+
+const myWeek = [
+  {
+    day: "Monday",
+    activity: "Reading",
+    category: "creative",
+    hoursSpent: 1,
+    enjoyment: 8,
+    timeOfDay: "afternoon"
+  },
+  {
+    day: "Tuesday",
+    activity: "Hiking",
+    category: "physical",
+    hoursSpent: 2,
+    enjoyment: 9,
+    timeOfDay: "morning"
+  },
+  {
+    day: "Wednesday",
+    activity: "Baking",
+    category: "creative",
+    hoursSpent: 2,
+    enjoyment: 8,
+    timeOfDay: "afternoon"
+  },
+  {
+    day: "Thursday",
+    activity: "Gym",
+    category: "physical",
+    hoursSpent: 1,
+    enjoyment: 5,
+    timeOfDay: "morning"
+  },
+  {
+    day: "Friday",
+    activity: "Walking",
+    category: "physical",
+    hoursSpent: 2,
+    enjoyment: 7,
+    timeOfDay: "afternoon"
+  },
+  {
+    day: "Saturday",
+    activity: "Yoga",
+    category: "physical",
+    hoursSpent: 1,
+    enjoyment: 7,
+    timeOfDay: "afternoon"
+  },
+  {
+    day: "Sunday",
+    activity: "Meet with friends",
+    category: "social",
+    hoursSpent: 2,
+    enjoyment: 8,
+    timeOfDay: "afternoon"
+  }
+];
